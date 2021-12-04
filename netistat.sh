7266e61e900ca97fdb105c6e8a7e4b6be7e2c4b6
Tiago <tiago_pt@protonmail.com>
2021-12-04 00:15:07 +0000

    Implementacao da opcoes, com execao da -c

--- netistat.sh
+++ netistat.sh
@@ -1,16 +1,9 @@
-#!/bin/bash
 # Vai bustar o fincheiro para o print inicial
 
-#inclui o ficheiro para printar com fonts
-#DIR="${BASH_SOURCE%/*}"
-#if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
-#. "$DIR/fonts.sh"
-
-#exitcode=$?
 
 assert() { #verifica a quantidade de argumentos
     re='^[0-9]+$'
-    if ! [[ $s =~ $re ]]; then # se o primeiro argumento nao for um numero e o numero total de argumentos nao for igual ou superior a 2 o programa nao corre
+    if ! [[ $s =~ $re ]]; then # se o argumento do tempo nao for um numero e o numero total de argumentos nao for igual ou superior a 2 o programa nao corre
 
         if [[ $n_arg -lt 2 ]]; then
             echo "Assertion failed: parâmetro obrigatório em falta (Tempo em segundos )"
@@ -37,8 +30,6 @@ dicform_r=()
 
 
 function getinterfaces() {
-
-    # Add new element at the end of the array
     for inter in $(ifconfig -a | sed 's/[ \t].*//;/^$/d' | cut -d ":" -f1); do
         interfaces+=($inter)
     done
@@ -95,24 +86,20 @@ function print_dados() {
      
         let tx=tx_f-tx_i
 
-
+        let t_total=t_inicial+tx
+        let r_total=r_inicial+rx
         
 
-        
-        
     
         rr=$( bc <<< "scale=2; $rx / $s" )
         tr=$( bc <<< "scale=2; $tx / $s" )
 
-        let t_total=t_total+tx
-        let r_total=r_total+rx
+        
         
 
         case $opv in
             0)
                 if [[ loop -ne 0 ]]; then
-                    #rt=$(bc <<<"scale=2;$rt+$rx")
-                    #tt=$(bc <<<"scale=2;$rt+$rx")
                     printf "%-10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\n" "$i" "$tx B" "$rx B" "$tr B" "$rr B" "$t_total B" "$r_total B"
                 else
                     printf "%-10s\t%10s\t%10s\t%10s\t%10s\n" "$i" "$tx B" "$rx B" "$tr B" "$rr B"
@@ -128,8 +115,6 @@ function print_dados() {
                 tr=$(bc <<<"scale=2;$tr/1000000")
 
                 if [[ $loop -ne 0 ]]; then
-                    #rt=$(bc <<<"scale=2;$rt+$rx")
-                    #tt=$(bc <<<"scale=2;$rt+$rx")
                     printf "%-10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\n" "$i" "$tx B" "$rx B" "$tr B" "$rr B" "$t_total B" "$r_total B"
                 else
                     printf "%-10s\t%10s\t%10s\t%10s\t%10s\n" "$i" "$tx Mb" "$rx Mb" "$tx Mb" "$rr Mb"
@@ -144,17 +129,14 @@ function print_dados() {
                 tr=$(bc <<<"scale=2;$tr/1000")
 
                 if [[ $loop -ne 0 ]]; then
-                    #rt=$(bc <<<"scale=2;$rt+$rx")
-                    #tt=$(bc <<<"scale=2;$rt+$rx")
                     printf "%-10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\n" "$i" "$tx B" "$rx B" "$tr B" "$rr B" "$t_total B" "$r_total B"
                 else
                     printf "%-10s\t%10s\t%10s\t%10s\t%10s\n" "$i" "$tx Kb" "$rx Kb" "$tx Kb" "$rr Kb"
                 fi
         esac
-
         dicform_r[$i]=$(echo $t_total)
         dicform_r[$i]=":"$(echo $r_total)
-        
+
     done
    
 }
@@ -162,87 +144,195 @@ function print_dados() {
 # Imprime a tabela -- output
 
 
+n_arg=$#
+s=${@: -1}
+
 # Opções usadas
 order_of_sort=""
 reverse=0
-regex=""
+regex="cat"
 opv=0
-loop=1
-# Este comando executa antes de tudo lendo as opções -- A String começa sempre por :, e quando uma opção leva argumentos tbm como vemos na opção c
-while getopts :bc:lmrRtTvk option ; do
+loop=0
+head=
+p_size=1
+
+# Este comando executa antes de tudo lendo as opções -- A String começa sempre por :, e quando uma opção leva argumentos tbm como vemos na opção c. Além disso para sempre antes do primeiro argumento que ñ é uma opção
+while getopts bc:lmp:rRtTvk option ; do
     case $option in
-
-    b)
-
-        opv=0
-        ;;
-    c)
-        regex=$OPTARG
-        ;;
-    k)
-        opv=1
-        ;;
-    l)
-        loop=1
-        ;;
-    m)
-        opv=2
-        ;;
-    r)
-        order_of_sort="-k 2 -n"
-        ;;
-    R)
-        order_of_sort="-k 2 -n"
-        ;;
-    t)
-        order_of_sort="-k 2 -n"
-
-        ;;
-    T)
-        order_of_sort="-k 2 -n"
-        ;;
-
-    v)
-        reverse=1
-        ;;
-    
+    c)  regex="grep \"$(echo $OPTARG)\" " ;;
+    k)  opv=2;;
+    l)  loop=1;;
+    m)  opv=1;;
+    p)  n_head="-"$(echo $OPTARG) ;
+        p_size=$OPTARG
+    ;;
+    r)  order_of_sort="-k 3 -n";;
+    R)  order_of_sort="-k 5 -n";;
+    t)  order_of_sort="-k 2 -n";;
+    T)  order_of_sort="-k 4 -n";;
+    v)  reverse=1;;
     esac
 done
-# tempo intruduzido, argumento obrigatório, falta fazer o assert
-s=$1
 
+# Concatenação de  -r ás opções do sort
 
-assert
-# Dados iniciais
+if [[ reverse -eq 1 ]] ; then
+    order_of_sort=$(echo $order_of_sort)" -r"
+fi 
 
-getinterfaces
-for i in "${interfaces[@]}"; do
-    dicform_i[$i]=$(ifconfig $i | sort | grep packets | grep TX | awk '{print $5}')
-    dicform_i[$i]+=":"$(ifconfig $i | sort | grep packets | grep RX | awk '{print $5}')
-    dicform_r[$i]="0:0"
-done
-sleep $s
-while true ; do
-    print_dados
-    echo "------------"
+# Declaração de uma estrutura a que chamamos de dicform que é semelhante a um dicionário em python ou um Map em Java que é um array que aceita como indices strings
+declare -A dicform_i
+dicform_i=()
+declare -A dicform_f
+dicform_f=()
+
+declare -A dicform_r
+dicform_r=()
+
+function assert() { #verifica a quantidade de argumentos
+    re='^[0-9]+$'
+
+    if [ $n_arg -le 2 ]; then # ver se no minimo temos um argumento
+        echo "Assertion failed: parâmetro obrigatório em falta (Tempo em segundos )"
+        exit $E_ASSERT_FAILED
+    fi
+
+    
+    if ! [[ $s =~ $re ]]; then # se o argumento do tempo nao for um numero e o numero total de argumentos nao for igual ou superior a 2 o programa nao corre
+
+        if [[ $n_arg -lt 2 ]]; then
+            echo "Assertion failed: parâmetro obrigatório em falta (Tempo em segundos )"
+            exit $E_ASSERT_FAILED
+        fi
+    fi
+    
+    if [ $p_size -le 0 ]; then # ver se o numero de interfaces da opção p é maior que zero
+        echo "Option -p must have positive arguments"
+        exit $E_ASSERT_FAILED
+    fi
+}
+# Imprime dados -- Esta função está separada para facilmente ordenarmos os dados dando 'pipe' da função ao sort
+function print_dados() {
+    getdicform
+    for i in "${interfaces[@]}"; do
+    
+        linha_i="$i ${dicform_i[$i]}"
+        linha_f="$i ${dicform_f[$i]}"
+        linha_r="$i ${dicform_r[$i]}"
+
+        rx_i=$(echo $linha_i | grep $i | awk '{print $2}' | cut --complement -d ":" -f1)
+        tx_i=$(echo $linha_i | grep $i | awk '{print $2}' | cut -d ":" -f1)
+
+        r_inicial=$(echo $linha_r | grep $i | awk '{print $2}' | cut --complement -d ":" -f1)
+        t_inicial=$(echo $linha_r | grep $i | awk '{print $2}' | cut -d ":" -f1)
+        
+        rx_f=$(echo $linha_f | grep $i | awk '{print $2}' | cut --complement -d ":" -f1)
+        tx_f=$(echo $linha_f | grep $i | awk '{print $2}' | cut -d ":" -f1)
+
+        r_total=$(echo $linha_r | grep $i | awk '{print $2}' | cut --complement -d ":" -f1)
+        t_total=$(echo $linha_r | grep $i | awk '{print $2}' | cut -d ":" -f1)
+        
+        let rx=rx_f-rx_i     
+        let tx=tx_f-tx_i
+
+        let t_total=t_inicial+tx
+        let r_total=r_inicial+rx
+        
+        rr=$( bc <<< "scale=2; $rx / $s" )
+        tr=$( bc <<< "scale=2; $tx / $s" )
+
+        case $opv in # ver se é para imprimir em bytes/Mb ou Kb
+            0)
+                if [[ loop -ne 0 ]]; then
+                    printf "%-10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\n" "$i" "$tx B" "$rx B" "$tr B" "$rr B" "$t_total B" "$r_total B"
+                else
+                    printf "%-10s\t%10s\t%10s\t%10s\t%10s\n" "$i" "$tx B" "$rx B" "$tr B" "$rr B"
+                fi
+
+                ;;
+            1)
+                rx=$(bc <<<"scale=2;$rx/1000000")
+                tx=$(bc <<<"scale=2;$tx/1000000")
+
+                rr=$(bc <<<"scale=2;$rr/1000000")
+                tr=$(bc <<<"scale=2;$tr/1000000")
+
+                t_t=$(bc <<<"scale=2;$t_total/1000000")
+                r_t=$(bc <<<"scale=2;$r_total/1000000")
+
+                if [[ $loop -ne 0 ]]; then
+                    printf "%-10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\n" "$i" "$tx Mb" "$rx Mb" "$tr Mb" "$rr Mb" "$t_t Mb" "$r_t Mb"
+                else
+                    printf "%-10s\t%10s\t%10s\t%10s\t%10s\n" "$i" "$tx Mb" "$rx Mb" "$tx Mb" "$rr Mb"
+                fi
+
+                ;;
+            2)
+                rx=$(bc <<<"scale=2;$rx/1000")
+                tx=$(bc <<<"scale=2;$tx/1000")
+
+                rr=$(bc <<<"scale=2;$rr/1000")
+                tr=$(bc <<<"scale=2;$tr/1000")
+
+                t_t=$(bc <<<"scale=2;$t_total/1000")
+                r_t=$(bc <<<"scale=2;$r_total/1000")
+
+                if [[ $loop -ne 0 ]]; then
+                    printf "%-10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\n" "$i" "$tx Kb" "$rx Kb" "$tr Kb" "$rr Kb" "$t_t Kb" "$r_t Kb"
+                else
+                    printf "%-10s\t%10s\t%10s\t%10s\t%10s\n" "$i" "$tx Kb" "$rx Kb" "$tx Kb" "$rr Kb"
+                fi
+        esac
+
+        dicform_r[$i]=$(echo $t_total)
+        dicform_r[$i]=":"$(echo $r_total)
+
+    done
+   
+}
+
+function inicial_setup(){ #Setup inicial
+    assert
+    getinterfaces
     for i in "${interfaces[@]}"; do
         dicform_i[$i]=$(ifconfig $i | sort | grep packets | grep TX | awk '{print $5}')
         dicform_i[$i]+=":"$(ifconfig $i | sort | grep packets | grep RX | awk '{print $5}')
+        dicform_r[$i]="0:0"
     done
+    if [[ $loop -eq 0 ]] ; then
+        printf "%-10s\t%10s\t%10s\t%10s\t%10s\n\n" "NETIF" "TX" "RX" "TRATE" "RRATE"
+    else
+        printf "%-10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\n\n" "NETIF" "TX" "RX" "TRATE" "RRATE" "TXTOT" "RXTOT"
+    fi
     sleep $s
-done
+}
 
-# Exemplo de opções para o Sort
-options="-k 2 -n "
+function getinterfaces() { # Recolher as interfaces
+    for inter in $(ifconfig -a | sed 's/[ \t].*//;/^$/d' | cut -d ":" -f1); do
+        interfaces+=($inter)
+    done
+}
 
+function getdicform() { # Recolher os dados mais recentes das interfaces
+    for i in "${interfaces[@]}"; do
+        dicform_f[$i]=$(ifconfig $i | sort | grep packets | grep TX | awk '{print $5}')
+        dicform_f[$i]+=":"$(ifconfig $i | sort | grep packets | grep RX | awk '{print $5}')
+    done
+}
 
+inicial_setup
 
-# Extras
-
-# readarray var -- le cada linha para um elemento do array
-
-# ifconfig |sort -- para termos as interfaces sempre pela mesma ordem *| grep RX -- seleciona pela keyword RX | grep bytes -- "" "" bytes| awk '{print $5}' imprime a coluna 5
-# * eu queria ivitar isto mas a bash ñ suporta  arrays multidimensionais portanto tiro as interfaces e os dados sempre pela mesma ordem
-
-# ifconfig| sort |* grep -E 'inet|ether' -- Seleciona lihas com inet ou ether -v --faz a inversão da seleção | grep : | awk '{print $1}' | cut -d ":" -f1 -- corta o ultimo caracter ":"
-# * Uma maneira simples de selecionar as interfaces é pelos ":", no entanto os endereços Iv6 vem atrás com este comando livro-me dessas linha
+if [[ $loop -eq 1 ]]; then # parte do loop caso seja selecionada a opção -l
+    while true ; do
+        print_dados | sort $order_of_sort | head $n_head
+        for i in "${interfaces[@]}"; do
+            dicform_i[$i]=$(ifconfig $i | sort | grep packets | grep TX | awk '{print $5}')
+            dicform_i[$i]+=":"$(ifconfig $i | sort | grep packets | grep RX | awk '{print $5}')
+        done 
+        sleep $s
+        echo "----------------------------------------------------------------------------------------------------------"
+        printf "%-10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\n\n" "NETIF" "TX" "RX" "TRATE" "RRATE" "TXTOT" "RXTOT"
+    done
+else
+    print_dados |  sort $order_of_sort | head $n_head # parte não loop
+fi
